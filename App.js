947695ca3f3c1f156a55729535635493509b5a15
Danielle Letarte <DANIELLE.LETARTE@LIBERTYMUTUAL.COM>
2018-07-11 11:44:05 -0400

    Added Apollo support for querying backend. Tested fetch is working

--- App.js
+++ App.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>=^-^=</Text>
       </View>
     );
   }
 }
+
+const client = new ApolloClient({
+    uri: "http://localhost:4000/graphql"
+});
+
+client
+    .query({
+        query: gql`
+      {
+        getAllCats {
+          name
+          resident
+          age
+        }
+      }
+    `
+    })
+    .then(result => console.log(result));
 
 const styles = StyleSheet.create({
   container: {

e322769bbb1c337303f18babfbfa59270dd6842b
danielleletarte <DANIELLE.A.LETARTE@GMAIL.COM>
2018-07-24 12:49:03 -0400

    Refactored DailyHealth component to allow for management of state

--- src/components/DailyHealth.js
+++ src/components/DailyHealth.js
@@ -4,34 +4,46 @@ import { Card, Icon } from "react-native-elements";
 import HealthSelection from "./HealthSelection";
 import moment from "moment";
 
-const DailyHealth = props => (
-  <Card>
-    <View style={{ flexDirection: "row" }}>
-      <Text style={{ flex: 1, fontWeight: "bold" }}>
-        {props.healthData
-          ? moment(props.healthData.date).format("MMMM Do YYYY")
-          : moment().format("MMMM Do YYYY")}
-      </Text>
-      <Icon name="edit" size={20} />
-    </View>
-    <HealthSelection
-      task={props.healthData ? props.healthData.urinate : false}
-      edit={true}
-      text={"Urinate"}
-    />
-    <HealthSelection
-      task={props.healthData ? props.healthData.poo : false}
-      edit={false}
-      text={"Feces"}
-    />
-    <HealthSelection
-      task={props.healthData ? props.healthData.eat : false}
-      edit={false}
-      text={"Eat"}
-    />
-    <Text style={{ fontWeight: "bold" }}>Notes</Text>
-    <Text>{props.healthData ? props.healthData.notes : null}</Text>
-  </Card>
-);
+export default class DailyHealth extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editEnabled: false,
+      healthData: props.healthData
+    };
+  }
 
-export default DailyHealth;
+  render() {
+    return (
+      <Card>
+        <View style={{ flexDirection: "row" }}>
+          <Text style={{ flex: 1, fontWeight: "bold" }}>
+            {this.state.healthData
+              ? moment(this.state.healthData.date).format("MMMM Do YYYY")
+              : moment().format("MMMM Do YYYY")}
+          </Text>
+          <Icon name="edit" size={20} />
+        </View>
+        <HealthSelection
+          task={this.state.healthData ? this.state.healthData.urinate : false}
+          edit={true}
+          text={"Urinate"}
+        />
+        <HealthSelection
+          task={this.state.healthData ? this.state.healthData.poo : false}
+          edit={false}
+          text={"Feces"}
+        />
+        <HealthSelection
+          task={this.state.healthData ? this.state.healthData.eat : false}
+          edit={false}
+          text={"Eat"}
+        />
+        <Text style={{ fontWeight: "bold" }}>Notes</Text>
+        <Text>
+          {this.state.healthData ? this.state.healthData.notes : null}
+        </Text>
+      </Card>
+    );
+  }
+}
